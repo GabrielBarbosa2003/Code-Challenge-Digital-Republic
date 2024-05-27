80949fe13857d5d0d408acb6ee30b44248c75615
gabriel <gabrieldecarvalhu@gmail.com>
2024-05-27 11:32:57 -0300

    primeiro layout e regras, 01,02,03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,75 @@
+import { useState } from 'react';
 import './App.css'
+import './grid.css'
+import {verificaTamanhoParede, calculaPortaEJanela} from './services/services';
 
 function App() {
+  const [formData, setFormData] = useState({
+    altura: '',
+    largura: '',
+    janelas: '',
+    portas: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const altura = parseInt(formData.altura);
+    const largura = parseInt(formData.largura);
+    const janelas = parseInt(formData.janelas);
+    const portas = parseInt(formData.portas)
+
+    verificaTamanhoParede({altura, largura})
+    calculaPortaEJanela({altura, largura, janelas, portas})
+    //console.log('Dados do formulário:', formData);
+  };
+
 
   return (
     <>
-     <h1>oii</h1>
+      <div className='container'>
+        <div className='grid'>
+
+          <div className='grid_esquerda'>
+            <h1>Descubra a quantidade de tinta!</h1>
+            <h2> CODE CHALLENGE</h2>
+          </div>
+
+          <div className='grid_direita'>
+            <form className='formulario' onSubmit={handleSubmit}>
+              <h1>Preencha com os dados do comodo!</h1>
+
+              <label htmlFor="altura" >Altura parede 1:</label>
+              <input type='text' id='altura' name='altura'  value={formData.altura} onChange={handleChange} />
+
+              <label htmlFor="largura" >largura parede 1:</label>
+              <input type='text' id='largura' name='largura'  value={formData.largura} onChange={handleChange} />
+
+              <label htmlFor="janelas">Quantidade de janelas:</label>
+              <input type='text' id='janelas' name='janelas' value={formData.janelas} onChange={handleChange}/>
+
+              <label htmlFor="portas">Quantidade de portas:</label>
+              <input type='text' id='portas' name='portas' value={formData.portas} onChange={handleChange}/>
+
+              <button type="submit">Enviar</button>
+            </form>
+          </div>
+
+
+        </div>
+
+
+      </div>
+
     </>
   )
 }
---
+++ src/services/services.js
@@ -0,0 +1,37 @@
+ function verificaTamanhoParede({ altura, largura }) {
+
+     // regra de negocios 1: Nenhuma parede pode ter menos de 1 metro quadrado nem mais de 50 metros quadrados, mas podem possuir alturas e larguras diferentes
+     if (altura < 1 || largura < 1) {
+          throw new Error("O valor deve ser maior do que 1");
+     } else if (altura > 50 || largura > 50) {
+          throw new Error("Altura ou largura devem ser menores do que 50m");
+     } else return true;
+}
+
+ function calculaPortaEJanela({ altura, largura, janelas, portas }) {
+     altura = parseFloat(altura);
+     largura = parseFloat(largura);
+
+     const areaParede = altura * largura;
+     const areaPortas = portas * 0.8 * 1.9;
+     const areaJanelas = janelas * 2.0 * 1.2;
+     const areaTotalPortasJanelas = areaPortas + areaJanelas;
+
+     // regra de negocios 2: O total de área das portas e janelas deve ser no máximo 50% da área de parede
+     if (areaTotalPortasJanelas > areaParede / 2) {
+          throw new Error('A área total de portas e janelas deve ser no máximo 50% da área da parede.');
+     }
+
+     // Regra 3: A altura de paredes com porta deve ser, no mínimo, 30 centímetros maior que a altura da porta
+     if (portas > 0 && altura < 1.9 + 0.3) {
+          throw new Error('A altura da parede deve ser no mínimo 30 cm maior que a altura da porta (2.2 metros).');
+     }
+
+     return true;
+
+
+
+
+}
+
+export { verificaTamanhoParede, calculaPortaEJanela}
